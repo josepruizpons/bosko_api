558e5993dc386c3a7f0f47166bafcb1dbc3b81d3
Josep Ruiz <josep.ruiz@taptapdigital.com>
2026-02-08 18:29:50 +0100

    db connection

--- src/db.ts
+++ src/db.ts
@@ -5,6 +5,7 @@ import pg from 'pg'
 
 const { Pool } = pg
 
+console.log(process.env.DATABASE_URL)
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
