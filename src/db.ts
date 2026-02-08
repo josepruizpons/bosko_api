f74f2ed20ecc98e9933e58445e849ef643ebb1df
Josep Ruiz <josep.ruiz@taptapdigital.com>
2026-02-08 18:42:02 +0100

    db connection

--- src/db.ts
+++ src/db.ts
@@ -1,17 +1,10 @@
 import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from './generated/prisma/client'
-import pg from 'pg'
 
-const { Pool } = pg
+const connectionString = `${process.env.DATABASE_URL}`
 
-console.log(process.env.DATABASE_URL)
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-})
-
-const adapter = new PrismaPg(pool)
+const adapter = new PrismaPg({ connectionString })
 const db = new PrismaClient({ adapter })
 
 export { db }
